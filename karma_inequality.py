0a80279dc2dc7bcabfa9fcfc75c26a254ca8cf9f
Sergey <sergey.paramonov@phystech.edu>
2016-01-26 02:21:28 +0100

    updated user database script

--- karma_inequality.py
+++ karma_inequality.py
@@ -1,10 +1,83 @@
+# -*- coding: utf-8 -*-
+
 import pandas as pd
-#hist = data['karma'].plot(kind='hist', bins=1000)
-data  = pd.read_csv(" user_dataset.csv")
-karma = data['karma']
-def proportion_of_karma(x):
-  top_x_karma = karma[karma > karma.quantile(x)].sum()
+import numpy as np
+import matplotlib.pyplot as plt
+import imp
+import matplotlib
+from matplotlib import rc
+matplotlib.style.use('ggplot')
+rc('font',**{'family':'serif'})
+rc('text', usetex=True)
+rc('text.latex',unicode=True)
+rc('text.latex',preamble='\\usepackage[utf8]{inputenc}')
+rc('text.latex',preamble='\\usepackage[russian]{babel}')
+
+def main():
+
+    #hist = data['karma'].plot(kind='hist', bins=1000)
+    data  = pd.read_csv("user_dataset.csv")
+    print(data)
+    karma = data['karma']
+    pubs  = data['publications']
+    comments = data['comments']
+    favs     = data['favourites']
+#   get_karma_bar(karma)
+#   get_overall_distribution(pubs, u"cтатей по количеству пользователей", u"Количество публикаций", u"Количество таких пользователей")
+#   get_overall_distribution(karma, u"кармы по количеству пользователей", u"Карма", u"Количество таких пользователей")
+#   get_overall_distribution(comments, u"комментариев по количеству пользователей", u"Комментарии", u"Количество таких пользователей")
+    get_overall_distribution(favs, u"записей избранного по количеству пользователей", u"Избранное", u"Количество таких пользователей")
+
+
+def proportion_of_karma(karma, x, strict=False):
+  if strict:
+    top_x_karma = karma[karma > karma.quantile(x)].sum()
+  else:
+    top_x_karma = karma[karma >= karma.quantile(x)].sum()
   all_karma = karma.sum()
   return top_x_karma/all_karma
 
+def get_fraction_percentile(karma):
+    for x in np.arange(0,1,0.1):  
+        a = proportion_of_karma(karma,x,strict=True)
+        b = proportion_of_karma(karma,x+0.1,strict=True)
+        x_fraction = a - b
+        yield x_fraction
 
+def get_overall_distribution(x, param, xlabel, ylabel):
+
+    plt.figure()
+    N = 250
+#   plt.xlim((0,N))
+    plt.hist(x, bins=N, log=True)
+    plt.title(u"Распределение "+param, fontsize=27)
+    plt.tick_params(axis='both', which='major', labelsize=20)
+    plt.xlabel(xlabel, fontsize=23)
+    plt.ylabel(ylabel, fontsize=23)
+#   plt.xticks(per_ids+bar_width/2,('100-90%', '90-80%', '80-70%', '70-60%', '60-50%', '50-40%', '40-30%', '30-20%', '20-10%', '10%'))
+
+#   plt.ylabel("Доля от общей кармы", fontsize=23)
+    plt.show()
+    plt.close()
+
+
+
+def get_karma_bar(karma):
+    percentiles = list(get_fraction_percentile(karma))
+
+    plt.figure()
+    bar_width = 0.5
+    per_ids   = np.array((range(len(percentiles))))
+    plt.bar(per_ids, percentiles, bar_width)
+    plt.title("Сколько кармы приходится на i-ый перцентиль пользователей?", fontsize=27)
+    plt.tick_params(axis='both', which='major', labelsize=20)
+    plt.xlabel("Прослойка пользователей от топ X% до топ Y%. Последнее значение top X%", fontsize=23)
+    plt.xticks(per_ids+bar_width/2,('100-90%', '90-80%', '80-70%', '70-60%', '60-50%', '50-40%', '40-30%', '30-20%', '20-10%', '10%'))
+
+    plt.ylabel("Доля от общей кармы", fontsize=23)
+    plt.show()
+    fig.close()
+
+
+if __name__ == "__main__":
+    main()
